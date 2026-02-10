2661177b962bc674a629169a3cee76f33b419139
LuisanaC14 <luisana.yamile10@gmail.com>
2026-02-10 00:26:53 -0500

    Validacion correo unico forms

--- gestion/forms.py
+++ gestion/forms.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegistroForm(UserCreationForm):
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Este correo electrónico ya está registrado. Por favor usa otro.")
+        return email
+
     class Meta:
         model = User
         fields = ['username', 'email'] 
